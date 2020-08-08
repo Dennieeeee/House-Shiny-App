b0143a27f0a9a8b2e27931d641e45fe8596e935b
Dennieeeee <38169907+Dennieeeee@users.noreply.github.com>
2020-08-07 21:32:33 -0700

    Add files via upload

--- server.R
+++ server.R
@@ -1,73 +1,56 @@
 library(tidyverse)
 library(ggplot2)
-library(lubridate)
-library(corrplot)
-url='/Users/dennie/Desktop/NEU/ALY 6070/Module 5/House_Price_data.csv'
-data <- read.csv(url)
-names(data) <- tolower(names(data))
-
-#remove outlier on lotfrontage
-ggplot(data, aes(x = lotfrontage)) +
-  geom_boxplot()
-subset(data, lotfrontage > 150)
-remove <- which(data$lotfrontage>150)
-data<-data[-remove,]
-
-#remove outlier on lotarea
-ggplot(data, aes(x = lotarea)) +
-  geom_boxplot()
-subset(data, lotarea >60000)
-remove <- which(data$lotarea>60000)
-data<-data[-remove,] 
-
-#remove rows with missing data in lotfrontage
-remove <- which(is.na(data$lotfrontage))
-data <- data[-remove,]
-
-#add month and date columns and create a yearbuilt date column
-
-
-#get all numeric data into numericVar
-numericVar <- which(sapply(data, is.numeric))
-numericVar <- which(sapply(data, is.numeric))
-numericVarNames <- which(sapply(data, is.numeric))
-cat('There are', length(numericVar), 'numeric variables')
-#subset data - only include numeric variables
-data_numVar <- data[,numericVar]
-#correlation:
-cor_numVar <- cor(data_numVar, use='pairwise.complete.obs')
-cor_sorted <- as.matrix(sort(cor_numVar[,'saleprice'],decreasing = TRUE))
-cor_sorted <- as.matrix(sort(cor_numVar[,'saleprice'],decreasing = TRUE))
-CorHight <- names(which(apply(cor_sorted, 1, function(x) abs(x)>0.5)))
-cor_numVar <- cor_numVar[CorHight,CorHight]
-corrplot.mixed(cor_numVar, tl.col='black',tl.pos='lt',las=2)
-#######################################################################################
-#######################################################################################
 
 function(input, output) {
   selectedData <- reactive({
-    data <- data[data[,'yearbuilt'] %in% as.numeric(input$year)]
+    data2[, c(input$xcol, input$ycol)]
   })
   
-  #in More drop down meanu
+  output$linePlot <- renderPlot({
+    palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+              "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
+    
+    par(mar = c(5.1, 4.1, 0, 1))
+    plot(selectedData())
+    #points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+  })
+  ##############################################################################################################################
+  ##############################################################################################################################
+  
+  #in More drop down menu
   output$conditionPlot <- renderPlot ({
     
-    plot(data$salecondition,data$saleprice,
-         main='Price VS. Year Built',
-         xlab = data$salecondition, ylab = 'price')
+    g <- ggplot(data=data[!is.na(data$saleprice),], aes(x=salecondition, y=saleprice))+
+      geom_boxplot(aes(color=salecondition)) + labs(x='Sale Condition',y='House Prices',
+                                                    title = 'Sale Prices VS. Condition of Properties') +
+      theme(panel.border = element_blank(),
+            panel.grid = element_blank(),
+            plot.title = element_text(hjust = 0.5,size = 20,face = 'bold'),
+            )
+    #g<- g+ggtitle('Condition of Properties VS. Sale Prices')
+    g
   })
   
-  #in More drop down meanu
-  output$qualityPlot <- renderPlot ({
+  #in More drop down menu
+  output$qualityPlot1 <- renderPlot ({
+    ggplot(data=data[!is.na(data$saleprice),], aes(x=saletype, y=saleprice)) +
+      geom_boxplot()+
+      labs(x='Overall Quality',y='House Price',
+           title = 'House Prices in Different Types of Sale') +
+      theme(panel.border = element_blank(),
+            panel.grid = element_blank(),
+            plot.title = element_text(hjust = 0.5,size = 20,face = 'bold'))
+  })
+    
+  output$qualityPlot2 <- renderPlot ({
     ggplot(data=data[!is.na(data$saleprice),], aes(x=saletype, y=saleprice))+
-      geom_boxplot(col='blue') + labs(x='Overall Quality') +
+      geom_boxplot()+
+      geom_jitter(alpha=1,color='tomato')+
+      labs(x='Overall Quality') +
       theme(panel.border = element_blank(),
             panel.grid = element_blank())
     
   })
-    
-  #   data[, c("year", input$year), drop = FALSE]
-  # }, rownames = TRUE)
 
   output$scatterplot <- renderPlot({
     
@@ -96,5 +79,7 @@ function(input, output) {
       theme(panel.border = element_blank(),
             panel.grid = element_blank())
   })
+
+
 }
 
--- ui.R
+++ ui.R
@@ -1,33 +1,46 @@
 library(shiny)
-fluidPage(
+
+vars <- setdiff(names(data2), "mssubclass")
+
+pageWithSidebar(
   headerPanel('Housing Explorer'),
-
-  sidebarLayout(
-    sidebarPanel(
-      # Select a year
-      sliderInput('year',label='year built:',
-                  min=1872,max=2010,value = 2010,step = 1),
-      selectInput('var1','X Variables',numericVar),
-      
-      selectInput('var2','Y Variables',numericVar),
-      
-      numericInput('clusters','Cluster Count',3,min=1,max=9),
+  sidebarPanel(
+    helpText('Explore the relationships between each features by selecting X and Y variables in the drop down meanu below'),
+    selectInput('xcol', 'X Variable', vars, selected = vars[[9]]),
+    selectInput('ycol', 'Y Variable', vars, selected = vars[[2]]),
   ),
-
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Scatter", plotOutput("scatterplot")),
-        tabPanel("Line", plotOutput("lineplot")),
-        # fluidRow(plotOutput('plot1'),
-        #          plotOutput('plot2')),
-        tabPanel("Number of Houses", plotOutput("plot1"),plotOutput("plot2")),
-        navbarMenu("More",
-          tabPanel("Quality and Price", plotOutput("qualityPlot")),
-          tabPanel("Conditions and Price", plotOutput("conditionPlot"))
-        )
+  mainPanel(
+    tabsetPanel(
+      tabPanel('Introduction',
+               tags$div(
+                 tags$h4('This project will be focusing on getting a general idea of houses that were 
+                 built between 1872 and 2010 in terms of the prices, etc.
+                 The purpose is to present the key information for people who want to buy a house 
+                 that was built prior to 2010. 
+                 Initially, I will analyze how lot size, sales price, and other variables changed throughout years. Next, I will
+                 look into lot size and sale price of the houses located around the top 3 most expensive neighborhoods. My audiences
+                 will also be able to gain a better understanding of how many houses were built each year, how many got remodeled, 
+                 and much more.'),
+                 tags$br(),
+                 tags$h4("The business questions are:"),
+                 tags$br(),
+                 tags$h5(
+                   tags$ol(
+                     tags$li("How did the lot size of houses change throughout years?"), 
+                     tags$li("What is the relationship between lot size and sales price of houses in different neighberhoods?"), 
+                     tags$li("How many houses were built each year and how many houses got remodeled each year?") 
+                   )
+                 ),
+               ),
+      ),
+      
+      tabPanel('Line',plotOutput('linePlot')),
+      tabPanel("Scatter", plotOutput("scatterplot")),
+      tabPanel("Number of Houses", plotOutput("plot1"),plotOutput("plot2")),
+      navbarMenu("More",
+                 tabPanel("Quality and Price", plotOutput("qualityPlot1"),plotOutput("qualityPlot2")),
+                 tabPanel("Conditions and Price", plotOutput("conditionPlot"))
       )
     )
   )
 )
-
-
